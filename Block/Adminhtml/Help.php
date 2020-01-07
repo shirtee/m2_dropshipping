f480e80e7ae0436aeeac37ff7be730272a9e1954
iFlair Bhavin <bhavin.bhalodia@iflair.com>
2020-01-07 17:43:36 +0530

    Update help section

--- Block/Adminhtml/Help.php
+++ Block/Adminhtml/Help.php
@@ -23,18 +23,18 @@ class Help extends \Magento\Backend\Block\AbstractBlock implements \Magento\Fram
 <div style="border:1px solid #ccc; padding:15px; text-align:center;">
 <img src="$logopath" width="200"/>
 <p>
-<strong>Shirtee Dropshipping Module v$version</strong><br />
-Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+<strong>Dropshipping Module v$version</strong><br />
+Design and sell your own apparel collections without any upfront costs!
 </p><br />
 <p>
-Website: <a href="https://www.shirtee.com" target="_blank">www.shirtee.com</a>
+Website: <a href="https://shirtee.cloud" target="_blank">shirtee.cloud</a>
 <br />Like, share and follow us on 
 <a href="https://www.facebook.com/shirtee.de/" target="_blank">Facebook</a>, 
 <a href="https://www.youtube.com/channel/UCMngeJszAvtvIEYoAk-z4pw" target="_blank">Youtube</a>, 
 <a href="https://www.instagram.com/shirtee.de/" target="_blank">Instagram</a>, and 
 <a href="https://de.pinterest.com/shirteeshirts/" target="_blank">Pinterest</a>.<br />
 If you have any questions send email at 
-<a href="mailto:magento@shirtee.com">magento@shirtee.com</a>.
+<a href="mailto:shops@shirtee.cloud">shops@shirtee.cloud</a>
 </p>
 </div>
 HTML;
